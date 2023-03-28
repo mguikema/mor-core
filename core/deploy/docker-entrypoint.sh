193e176c0dd9f9c0ea95e8f4feb112179f0a2df1
Maurice Guikema <mguikema@gmail.com>
2023-03-28 12:31:57 +0200

    model optimized

--- core/deploy/docker-entrypoint.sh
+++ core/deploy/docker-entrypoint.sh
@@ -9,15 +9,11 @@ python manage.py collectstatic --no-input
 # if [[ ${CLEAR_DATABSE_TABLES:-0} == 1 ]]; then
   echo "Start with a fresh database"
   export PGPASSWORD=${DATABASE_PASSWORD}
-  psql -h ${DATABASE_HOST} -p 5432 -d ${DATABASE_NAME} -U ${DATABASE_USER} -c "drop schema public cascade;"
-  psql -h ${DATABASE_HOST} -p 5432 -d ${DATABASE_NAME} -U ${DATABASE_USER} -c "create schema public;"
+  psql -h ${DATABASE_HOST_OVERRIDE} -p 5432 -d ${DATABASE_NAME} -U ${DATABASE_USER} -c "drop schema public cascade;"
+  psql -h ${DATABASE_HOST_OVERRIDE} -p 5432 -d ${DATABASE_NAME} -U ${DATABASE_USER} -c "create schema public;"
 # fi
 
 echo Apply migrations
 python manage.py migrate --noinput
-
-chmod -R ugo+rwx /srv/web/var/cache
-
-chmod -R 777 /static
 
 exec uwsgi --ini /app/deploy/config.ini
