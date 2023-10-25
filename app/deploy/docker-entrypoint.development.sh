4900e7b547adef514111baab5787e4259b494489
Jorrit Strikwerda <jippiejorrit@gmail.com>
2023-10-25 16:03:28 +0200

    Add create_applicaties command to docker-entrypoints

--- app/deploy/docker-entrypoint.development.sh
+++ app/deploy/docker-entrypoint.development.sh
@@ -30,6 +30,10 @@ fi
 echo Create users
 python manage.py createusers --noinput || true
 
+# Create applicaties for app to app communication
+echo Create applicaties
+python manage.py create_applicaties
+
 # Log a message indicating the script has completed
 echo "Docker development entrypoint script has completed."
 
