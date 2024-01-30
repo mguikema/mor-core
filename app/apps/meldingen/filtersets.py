63b3f026984b1f51d581553c17aba72e034a1f4b
Maurice Guikema <maurice@mgui.nl>
2024-01-30 20:38:33 +0100

    fix: meldr query search

--- app/apps/meldingen/filtersets.py
+++ app/apps/meldingen/filtersets.py
@@ -186,7 +186,7 @@ class MeldingFilter(BasisFilter):
             return queryset.filter(
                 Q(meta__meldingsnummerField__iregex=value)
                 | Q(meta__morId__iregex=value)
-                | Q(signalen_voor_melding__externe_bron_melding_id__iregex=value)
+                | Q(signalen_voor_melding__bron_signaal_id__iregex=value)
             ).distinct()
         return queryset
 
