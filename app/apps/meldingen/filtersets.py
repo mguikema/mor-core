cfec76a40ff74d3af37575cb5bf9d59f2c72cf3b
Jorrit Strikwerda <jippiejorrit@gmail.com>
2024-02-01 11:52:44 +0100

    Removed meta straatnaam query, added signalen_voor_melding.locaties_voor_signaal.straatnaam query.

--- app/apps/meldingen/filtersets.py
+++ app/apps/meldingen/filtersets.py
@@ -197,8 +197,10 @@ class MeldingFilter(BasisFilter):
                     | Q(meta__melderTelefoonField__iregex=term)
                     | Q(meta__melderEmailField__iregex=term)
                     | Q(meta__melderNaamField__iregex=term)
-                    | Q(meta__straatnaamField__iregex=term)
                     | Q(locaties_voor_melding__straatnaam__iregex=term)
+                    | Q(
+                        signalen_voor_melding__locaties_voor_signaal__straatnaam__iregex=term
+                    )
                     # B&C fields
                     | Q(meta__email_melder__iregex=term)
                     | Q(meta__telefoon_melder__iregex=term)
