6596eecf444376a7fe2b1c382eb0548416efaed8
Jorrit Strikwerda <j.strikwerda@alteza.nl>
2024-07-30 10:49:04 +0200

    Resolutie naar taakgebeurtenis

--- app/apps/meldingen/querysets.py
+++ app/apps/meldingen/querysets.py
@@ -25,36 +25,38 @@ class MeldingQuerySet(QuerySet):
         from apps.aliassen.models import OnderwerpAlias
         from apps.locatie.models import Locatie
 
-        locaties = Locatie.objects.filter(melding=OuterRef("pk")).order_by("-gewicht")
-        onderwerpen = OnderwerpAlias.objects.filter(
+        meldingen = self.prefetch_related("onderwerpen", "locatis_voor_melding")
+        locaties_subquery = Locatie.objects.filter(melding=OuterRef("pk")).order_by(
+            "-gewicht"
+        )[:1]
+        onderwerpen_subquery = OnderwerpAlias.objects.filter(
             meldingen_voor_onderwerpen=OuterRef("pk")
-        )
-        meldingen = self.all()
+        ).values("response_json__name")[:1]
 
-        logger.info(f"filtered meldingen count: {meldingen.count()}")
-
-        meldingen = meldingen.annotate(
-            onderwerp=Coalesce(
-                Subquery(onderwerpen.values("response_json__name")[:1]),
-                Value("Onbekend", output_field=models.JSONField()),
-            )
-        ).annotate(
-            wijk=Coalesce(
-                Subquery(locaties.values("wijknaam")[:1]),
-                Value("Onbekend"),
-            )
-        )
-        meldingen = meldingen.annotate(
-            onderwerp_wijk=Concat(
-                "onderwerp", Value("-"), "wijk", output_field=models.CharField()
-            )
-        )
         meldingen = (
-            meldingen.values("onderwerp_wijk", "onderwerp", "wijk")
+            meldingen.annotate(
+                onderwerp=Coalesce(
+                    Subquery(onderwerpen_subquery),
+                    Value("Onbekend", output_field=models.JSONField()),
+                )
+            )
+            .annotate(
+                wijk=Coalesce(
+                    Subquery(locaties_subquery.values("wijknaam")),
+                    Value("Onbekend"),
+                )
+            )
+            .annotate(
+                onderwerp_wijk=Concat(
+                    "onderwerp", Value("-"), "wijk", output_field=models.CharField()
+                )
+            )
             .order_by("onderwerp_wijk")
-            .annotate(count=Count("onderwerp_wijk"))
-            .values("count", "onderwerp", "wijk")
+            .values("onderwerp_wijk", "onderwerp", "wijk")
         )
-        meldingen_count = [m.get("count") for m in meldingen]
-        logger.info(f"meldingen count sum: {sum(meldingen_count)}")
+
+        meldingen = meldingen.annotate(count=Count("id")).values(
+            "count", "onderwerp", "wijk"
+        )
+
         return meldingen
--- app/apps/meldingen/serializers.py
+++ app/apps/meldingen/serializers.py
@@ -20,7 +20,6 @@ from drf_spectacular.utils import extend_schema_field
 from drf_writable_nested.serializers import WritableNestedModelSerializer
 from rest_framework import serializers
 from rest_framework.reverse import reverse
-from shapely.wkt import loads
 
 
 class MeldingLinksSerializer(serializers.Serializer):
@@ -364,19 +363,28 @@ class MeldingDetailSerializer(MeldingSerializer):
 
 
 class MeldingAantallenSerializer(serializers.Serializer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.wijken_gps_lookup = self.fetch_wijken_gps_lookup()
+
+    def fetch_wijken_gps_lookup(self):
+        gemeentecode = settings.WIJKEN_EN_BUURTEN_GEMEENTECODE
+        wijken = PDOKService().get_wijken_middels_gemeentecode(gemeentecode)
+        return {wijk.get("wijknaam"): wijk.get("centroide_ll") for wijk in wijken}
+
     def to_representation(self, instance):
-        wijken = PDOKService().get_wijken_middels_gemeentecode(
-            gemeentecode=settings.WIJKEN_EN_BUURTEN_GEMEENTECODE
-        )
-        wijken_gps_lookup = {
-            wijk.get("wijknaam"): wijk.get("centroide_ll") for wijk in wijken
-        }
         wijk = instance.get("wijk")
-        gps = loads(wijken_gps_lookup.get(wijk)) if wijken_gps_lookup.get(wijk) else ""
-        instance.update(
-            {
-                "lat": str(gps.coords[0][1]) if gps else "",
-                "lon": str(gps.coords[0][0]) if gps else "",
-            }
-        )
-        return instance
+        gps = self.wijken_gps_lookup.get(wijk)
+
+        if gps:
+            lat = str(gps.coords[0][1])
+            lon = str(gps.coords[0][0])
+        else:
+            lat = ""
+            lon = ""
+
+        return {
+            **instance,
+            "lat": lat,
+            "lon": lon,
+        }
