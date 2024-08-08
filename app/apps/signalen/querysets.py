72c3938f6ed9cf3dff53ad1e99bdee9083b97569
Maurice Guikema <maurice@mgui.nl>
2024-08-08 17:53:30 +0200

    aantallen for signalen

--- app/apps/signalen/querysets.py
+++ app/apps/signalen/querysets.py
@@ -21,27 +21,6 @@ logger = logging.getLogger(__name__)
 
 
 class SignaalQuerySet(QuerySet):
-    # def get_aantallen(self):
-    #     from apps.locatie.models import Locatie
-
-    #     locaties = Locatie.objects.filter(pk=OuterRef("pk")).order_by("-gewicht")
-    #     signalen = (
-    #         self.values(
-    #             onderwerp=F("onderwerpen__response_json__name"),
-    #         )
-    #         .filter(onderwerp__isnull=False)
-    #         .annotate(
-    #             wijk=Coalesce(
-    #                 Subquery(locaties.values("wijknaam")[:1]),
-    #                 Value("Onbekend"),
-    #             )
-    #         )
-    #         .values("onderwerp", "wijk")
-    #         .annotate(count=Count("onderwerp"))
-    #         .values("count", "onderwerp", "wijk")
-    #     )
-    #     return signalen
-
     def get_aantallen(self):
         from apps.aliassen.models import OnderwerpAlias
         from apps.locatie.models import Locatie
