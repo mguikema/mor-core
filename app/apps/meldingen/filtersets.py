68ddd55c3b8be6a9c48c0f2038891a509e46ace8
Jorrit Strikwerda <j.strikwerda@alteza.nl>
2024-08-20 12:19:36 +0200

    Remove old get_q

--- app/apps/meldingen/filtersets.py
+++ app/apps/meldingen/filtersets.py
@@ -221,40 +221,6 @@ class MeldingFilter(BasisFilter):
         return queryset
 
     # Er kan op meerdere komma separated zoektermen gezocht worden
-    def get_q_old(self, queryset, name, value):
-        if value:
-            search_terms = value.split(",")
-            combined_q = Q()
-
-            for term in search_terms:
-                term = term.strip()
-                combined_q &= (
-                    # MeldR-nummer fields
-                    Q(meta__meldingsnummerField__iregex=term)
-                    # | Q(meta__morId__iregex=term) Not used, previously needed for msb import meldingen
-                    | Q(signalen_voor_melding__bron_signaal_id__iregex=term)
-                    # Melder fields
-                    | Q(signalen_voor_melding__melder__naam__iregex=term)
-                    # | Q(signalen_voor_melding__melder__voornaam__iregex=term) Currently not used
-                    # | Q(signalen_voor_melding__melder__achternaam__iregex=term) Currently not used
-                    | Q(signalen_voor_melding__melder__email__iregex=term)
-                    | Q(signalen_voor_melding__melder__telefoonnummer__iregex=term)
-                    | Q(locaties_voor_melding__straatnaam__iregex=term)
-                    | Q(
-                        signalen_voor_melding__locaties_voor_signaal__straatnaam__iregex=term
-                    )
-                    # Old meta fields
-                    # | Q(meta__email_melder__iregex=term)
-                    # | Q(meta__telefoon_melder__iregex=term)
-                    # | Q(meta__naam_melder__iregex=term)
-                    # | Q(meta__melderTelefoonField__iregex=term)
-                    # | Q(meta__melderEmailField__iregex=term)
-                    # | Q(meta__melderNaamField__iregex=term)
-                )
-            return queryset.filter(combined_q).distinct()
-
-        return queryset
-
     def get_q(self, queryset, name, value):
         if value:
             # Annotate the queryset with a combined address field
