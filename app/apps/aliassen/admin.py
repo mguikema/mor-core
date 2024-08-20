fc35946f2c6c238c2a73c87e9a9305936469bfe4
Jorrit Strikwerda <j.strikwerda@alteza.nl>
2024-08-20 10:32:06 +0200

    Add naam custom admin field to OnderwerpAliasAdmin.

--- app/apps/aliassen/admin.py
+++ app/apps/aliassen/admin.py
@@ -3,7 +3,12 @@ from django.contrib import admin
 
 
 class OnderwerpAliasAdmin(admin.ModelAdmin):
-    list_display = ("id", "bron_url", "aangemaakt_op")
+    list_display = ("id", "bron_url", "naam", "aangemaakt_op")
+
+    def naam(self, obj):
+        return obj.response_json.get("name", "")
+
+    naam.short_description = "Naam"
 
 
 admin.site.register(OnderwerpAlias, OnderwerpAliasAdmin)
