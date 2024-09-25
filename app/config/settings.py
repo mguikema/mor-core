9b15ebd1ca8283b6bf22ebeae2b8020913b7a8ac
Maurice Guikema <maurice@mgui.nl>
2024-09-25 17:09:04 +0200

    token auth from readonly db

--- app/config/settings.py
+++ app/config/settings.py
@@ -244,9 +244,7 @@ REST_FRAMEWORK = dict(
     ],
     DEFAULT_SCHEMA_CLASS="drf_spectacular.openapi.AutoSchema",
     DEFAULT_PERMISSION_CLASSES=("rest_framework.permissions.IsAuthenticated",),
-    DEFAULT_AUTHENTICATION_CLASSES=(
-        "rest_framework.authentication.TokenAuthentication",
-    ),
+    DEFAULT_AUTHENTICATION_CLASSES=("apps.authenticatie.auth.TokenAuthentication",),
     EXCEPTION_HANDLER="utils.exception_handlers.api_exception_handler",
 )
 
